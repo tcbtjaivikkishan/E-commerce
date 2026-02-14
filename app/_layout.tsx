70d5bf8727dcfa0d648fef56bfe84033c941aaa4
piyush31508 <chawlapiyush811@gmail.com>
2026-02-14 14:50:07 +0530

    Setup Native-Wind and redux

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
+import { Provider } from "react-redux";
+import { store } from "../src/store/store";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <Provider store={store}>
+        <Stack />;
+      </Provider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 items-center justify-center bg-red-600 "
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-white">this screen.</Text>
     </View>
   );
 }
